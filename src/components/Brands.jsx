d61a5ae29e7dc4a0170d072348afa2395c667494
William-Mou <william.mou1024@gmail.com>
2024-11-12 22:25:03 +0800

    Init for 2025 winter
    Signed-off-by: William-Mou <william.mou1024@gmail.com>

---
+++ src/components/Brands.jsx
@@ -0,0 +1,101 @@
+import { motion } from "framer-motion";
+
+import SC_logo from "../assets/logos/SC_logo.png"
+import NCHC_logo from "../assets/logos/NCHC_logo.png"
+import NTHU_logo from "../assets/logos/NTHU_logo.png"
+import NVIDIA_logo from "../assets/logos/NVIDIA_logo.png"
+import AiMS_logo from "../assets/logos/AiMS_logo.png"
+
+
+export const Brands = () => (
+  <section className="py-12 sm:py-24 bg-customDarkBg1 w-full mt-16 mb-16"
+    id="brands">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5, delay: 0.2 }}
+    >
+      <div className="container px-4 mx-auto 2xl:w-[1200px] xl:w-[1100px] lg:w-[1000px] md:w-4/5 pt-20">
+        <div className="flex lg:flex-row flex-col items-center -mx-4 justify-center lg:text-left text-center">
+          <div className="w-full lg:w-1/2 px-4 mb-12 lg:mb-0">
+            <div className="flex flex-col">
+              <h2 className="mb-2 pb-10 text-4xl sm:text-5xl 2xl:text-6xl py-3 font-bold tracking-normal text-white">
+                HPC x AI <br />
+                高速計算人工智慧<br />
+                夏令營
+              </h2>
+              <h2 className=" text-4xl  sm:text-5xl 2xl:text-6xl font-bold tracking-normal text-customSecondary">
+                辦理單位
+              </h2>
+            </div>
+          </div>
+          <div className="w-2/3 sm:w-[620px] lg:w-1/2 mx-auto lg:mx-0 lg:pl-10">
+            <div className="flex flex-wrap -m-4">
+              <div className="flex py-3 justify-center">
+                <a href="https://www.nchc.org.tw/" target="_blank" rel="noopener noreferrer">
+                  <img
+                    src={NCHC_logo}
+                    alt="NCHC_logo"
+                    className="rounded-xl  custom-border-gray"
+                  />
+                </a>
+              </div>
+              <div className="flex py-3 justify-center">
+                <a href="http://scc.nthu.site/" target="_blank" rel="noopener noreferrer">
+                  <img
+                    src={SC_logo}
+                    alt="SC_logo"
+                    className="rounded-xl  custom-border-gray"
+                  />
+                </a>
+              </div>
+              <div className="flex py-3 justify-center">
+                <a href="https://www.nthu.edu.tw/" target="_blank" rel="noopener noreferrer">
+                  <img
+                    src={NTHU_logo}
+                    alt="NTHU_logo"
+                    className="rounded-xl custom-border-gray"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container px-4 mx-auto 2xl:w-[1200px] xl:w-[1100px] lg:w-[1000px] md:w-4/5 pt-20">
+        <div className="flex lg:flex-row flex-col items-center -mx-4 justify-center lg:text-left text-center">
+          <div className="w-full lg:w-1/2 px-4 mb-12 lg:mb-0">
+            <div className="flex flex-col">
+              <h2 className=" text-4xl  sm:text-5xl 2xl:text-6xl font-bold tracking-normal text-customSecondary">
+                贊助單位
+              </h2>
+            </div>
+          </div>
+          <div className="w-2/3 sm:w-[620px] lg:w-1/2 mx-auto lg:mx-0 lg:pl-10">
+            <div className="flex flex-wrap -m-4">
+              <div className="flex py-3 justify-center">
+                <a href="https://www.nvidia.com/" target="_blank" rel="noopener noreferrer">
+                  <img
+                    src={NVIDIA_logo}
+                    alt="NVIDIA_logo"
+                    className="rounded-xl  custom-border-gray"
+                  />
+                </a>
+              </div>
+              <div className="flex py-3 justify-center">
+                <a href="https://www.aims.org.tw/" target="_blank" rel="noopener noreferrer">
+                  <img
+                    src={AiMS_logo}
+                    alt="AiMS_logo"
+                    className="rounded-xl  custom-border-gray"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  </section>
+);
---
+++ src/components/Timetable.jsx
@@ -0,0 +1,160 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+import { InvitationModal } from "./InvitationModal";
+// import featuresdiagonal from "../assets/images/featuresdiagonal.jpg";
+import "../styles/Table.css";
+
+export const Timetable = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <section className="lg:mb-16 w-full flex flex-col justify-center items-center bg-customDarkBg1"
+      id="course">
+
+      <div className="custom-shape-divider-bottom-1665696614">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+          className="custom-bg-dark2"
+        >
+          <path
+            d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
+            className="custom-bg-dark1"
+          ></path>
+        </svg>
+      </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        className="w-full"
+      >
+        <div className="w-full max-w-[1150px] flex justify-center bg-customDarkBg1 pt-12 lg:pt-12 pb-4 lg:pb-10 mx-auto flex-col">
+          <div className="w-full flex flex-col items-center">
+            <h2 className="mt-10 mb-8 text-4xl lg:text-5xl custom-block-big-title">
+              專業知識密集課程
+            </h2>
+          </div>
+          <div className="w-full overflow-x-auto"> {/* 添加 overflow-x-auto */}
+            <MyTable />
+          </div>
+        </div>
+      </motion.div>
+      <div className="custom-shape-divider-top-1665696661 w-full">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+          className="custom-bg-dark2"
+        >
+          <path
+            d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
+            className="custom-bg-dark1"
+          ></path>
+        </svg>
+      </div>
+      {isModalOpen && (
+        <InvitationModal isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
+      )}
+    </section>
+  );
+};
+
+
+const MyTable = () => {
+  return (
+    <table className="GeneratedTable">
+      <thead>
+        <tr>
+          <th> </th>
+          <th colSpan="2" align="center">Day1.  7/28 (日)</th>
+          <th colSpan="2" align="center">Day2.  7/29 (一)</th>
+          <th colSpan="1" align="center">Day3.  7/30 (二)</th>
+          <th colSpan="1" align="center">Day4.  7/31 (三)</th>
+          <th colSpan="1" align="center">Day5.  8/1 (四)</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+            <td align="center">活動地點</td>
+            <td align="center">綜四223</td>
+            <td align="center">綜四224</td>
+            <td align="center">綜四223</td>
+            <td align="center">綜四224</td>
+            <td align="center">綜四224</td>
+            <td align="center">綜四224</td>
+            <td align="center">資電館107、國網中心 </td>
+        </tr>
+        <tr>
+            <td align="center">9:00~9:30</td>
+            <td colSpan="2" align="center">報到</td>
+            <td colSpan="2" rowSpan="2" align="center">超級電腦簡介與實操</td>
+            <td rowSpan="2" align="center">業界神秘嘉賓#2</td>
+            <td colSpan="1" rowSpan="3" align="center">深度學習簡介</td>
+            <td colSpan="1" rowSpan="3" align="center">競賽成果發表 </td>
+        </tr>
+        <tr>
+            <td align="center">9:30~10:30</td>
+            <td colSpan="2" align="center">開幕</td>
+        </tr>
+        <tr>
+            <td align="center">10:30~12:00</td>
+            <td colSpan="2" align="center">平行計算簡介(基礎)</td>
+            <td colSpan="2" align="center">業界神秘嘉賓#1</td>
+            <td align="center">效能分析</td>
+        </tr>
+        <tr>
+            <td align="center">12:00~13:30</td>
+            <td colSpan="7" align="center">午餐 (自理)</td>
+        </tr>
+        <tr>
+            <td align="center">13:30~15:00</td>
+            <td colSpan="2" align="center">Python環境安裝簡介</td>
+            {/* <td align="center">平行計算簡介(進階)</td> */}
+            <td colSpan="2" align="center">學生叢集競賽座談會</td>
+            <td align="center">效能競賽</td>
+            <td rowSpan="1" align="center">科學計算簡介</td>
+            <td rowSpan="2" align="center">國網中心參訪</td>
+        </tr>
+        <tr>
+            <td align="center">15:00~16:00</td>
+            <td colSpan="2" rowSpan="2" align="center">平行程式(CPU)</td>
+            <td colSpan="2" rowSpan="2" align="center">平行程式(GPU)</td>
+            <td rowSpan="2" align="center">程式競賽</td>
+            <td align="center">人工智慧簡介</td>
+        </tr>
+        <tr>
+            <td align="center">16:00~17:30</td>
+            <td align="center">AI/科學應用競賽</td>
+            <td align="center">返校、競賽頒獎 </td>
+        </tr>
+        <tr>
+            <td align="center">17:30~19:00</td>
+            <td colSpan="2" align="center"><em>Pizza, my friends. 🤌🍕</em></td>
+            <td colSpan="1" align="center">系所介紹晚宴(限高中生)</td>
+            <td colSpan="3" align="center">晚餐(自理)</td>
+            <td rowSpan="2"> </td>
+        </tr>
+        <tr>
+            <td align="center">19:00~21:30</td>
+            <td colSpan="2" align="center">平行程式實操(CPU)</td>
+            <td align="center">平行程式實操(GPU)</td>
+            <td align="center">進階平行程式實操(GPU)</td>
+            <td align="center">程式競賽</td>
+            <td align="center">AI/科學應用競賽</td>
+        </tr>
+      </tbody>
+    </table>
+
+
+
+
+  );
+};
+
+export default MyTable;
